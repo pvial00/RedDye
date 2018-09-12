f35cd29f5c7c1733aac81adf8c62a922437b7b31
Karl Zander <pvial@Karls-MacBook-Pro.local>
2018-09-12 16:42:29 -0700

    RedDye optimized

--- c/reddye.c
+++ c/reddye.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include "reddye_kdf.c"
 
 int keylen = 32;
 int k[32];
@@ -9,18 +10,18 @@ int j = 0;
 void keysetup(unsigned char *key, unsigned char *nonce) {
     int c;
     for (c=0; c < strlen(key); c++) {
-        k[c] = (k[c] + key[c]) % 256;
-        j = (j + k[c]) % 256; }
+        k[c] = (k[c] + key[c]) & 0xff;
+        j = (j + k[c]) & 0xff; }
     keylen = strlen(key);
     for (c = 0; c < 256; c++) {
-        k[c % keylen] = (k[c % keylen] + j) % 256;
-        j = (j + k[c % keylen]) % 256; }
+        k[c % keylen] = (k[c % keylen] + j) & 0xff;
+        j = (j + k[c % keylen]) & 0xff; }
     for (c = 0; c < strlen(nonce); c++) {
-        k[c] = (k[c] + nonce[c]) % 256;
+        k[c] = (k[c] + nonce[c]) & 0xff;
         j = (j + k[c]) % 256; }
     for (c = 0; c < 256; c++) {
-        k[c % keylen] = (k[c % keylen] + j) % 256;
-        j = (j + k[c % keylen]) % 256; }
+        k[c % keylen] = (k[c % keylen] + j) & 0xff;
+        j = (j + k[c % keylen]) & 0xff; }
 }
 
 
@@ -28,9 +29,9 @@ unsigned char * crypt(unsigned char *data, unsigned char *key, unsigned char *no
    keysetup(key, nonce);
    int c = 0;
    for (int x = 0; x < datalen; x++) {
-       k[c % keylen] = (k[c % keylen] + k[(c + 1) % keylen] + j) % 256;
-       j = (j + k[c % keylen] + c) % 256;
+       k[c % keylen] = (k[c % keylen] + k[(c + 1) % keylen] + j) & 0xff;
+       j = (j + k[c % keylen] + c) & 0xff;
        data[x] = data[x] ^ k[c % keylen];
-       c = (c + 1) % 256;
+       c = (c + 1) & 0xff;
    } 
 }
--- python/RedDye/redcrypt.py
+++ python/RedDye/redcrypt.py
@@ -1,7 +1,8 @@
-from RedDye import RedDye
+from reddye import RedDye
 import sys, select, getpass, os, time, getopt
 
 nonce_len = 16
+buf_len = 65536
 
 try:
     mode = sys.argv[1]
@@ -9,45 +10,97 @@ except IndexError as ier:
     print "Error: Did you forget encrypt/decrypt?"
     sys.exit(1)
 
-input_filename = sys.argv[2]
-output_filename = sys.argv[3]
+input_file = sys.argv[2]
+output_file = sys.argv[3]
 
-try:
-    infile = open(input_filename, "r")
-except IOError as ier:
-    print "Input file not found."
-    sys.exit(1)
-
-try:
-    outfile = open(output_filename, "w")
-except IOError as ier:
-    print "Output file not found."
-    sys.exit(1)
+fsize = os.path.getsize(input_file)
+if fsize < buf_len:
+    blocks = 0
+else:
+    blocks = fsize / buf_len
+extra = fsize % buf_len
+if extra != 0:
+    blocks += 1
 
 try:
     key = sys.argv[4]
 except IndexError as ier:
     key = getpass.getpass("Enter key: ")
 
-reddye = RedDye()
-key = reddye.kdf(key)
+def keysetup(key, nonce):
+    k = [0] * len(key)
+    j = 0
+    for c, char in enumerate(key):
+        k[c] = (k[c] + ord(char)) & 0xff
+        j = (j + k[c]) & 0xff
+    klen = len(k)
+    for c in range(256):
+        k[c % klen] = (k[c % klen] + j) & 0xff
+        j = (j + k[c % klen]) & 0xff
+    if nonce != "":
+        for c, char in enumerate(nonce):
+            k[c] = (k[c] + ord(char)) & 0xff
+            j = (j + k[c]) & 0xff
+        for c in range(256):
+            k[c % klen] = (k[c % klen] + j) & 0xff
+            j = (j + k[c % klen]) & 0xff
+    return k, j
 
-start = time.time()
-data = infile.read()
-infile.close()
+def encrypt(in_file, out_file, fsize, key):
+    infile = open(in_file, "r")
+    outfile = open(out_file, "w")
+    nonce = os.urandom(nonce_len)
+    outfile.write(nonce)
+    start = time.time()
+    k, j = keysetup(key, nonce)
+    klen = len(k)
+    c = 0
+    for x in range(blocks):
+        ctxt = []
+        block = infile.read(buf_len)
+        for b in block:
+            k[c % klen] = (k[c % klen] + k[(c + 1) % klen] + j) & 0xff
+            j = (j + k[c % klen] + c) & 0xff
+            sub = ((ord(b)) ^ k[c % klen])
+            ctxt.append(chr(sub))
+            c = (c + 1) & 0xff
+        outfile.write("".join(ctxt))
+    infile.close()
+    outfile.close()
+    end = time.time() - start
+    bps = fsize / end
+    sys.stdout.write("Completed in "+str(end)+" seconds\n")
+    sys.stdout.write(str(bps)+" bytes per second.\n")
+
+def decrypt(in_file, out_file, fsize, key):
+    infile = open(in_file, "r")
+    outfile = open(out_file, "w")
+    nonce = infile.read(nonce_len)
+    start = time.time()
+    k, j = keysetup(key, nonce)
+    klen = len(k)
+    c = 0
+    for x in range(blocks):
+        ctxt = []
+        block = infile.read(buf_len)
+        for b in block:
+            k[c % klen] = (k[c % klen] + k[(c + 1) % klen] + j) & 0xff
+            j = (j + k[c % klen] + c) & 0xff
+            sub = ((ord(b)) ^ k[c % klen])
+            ctxt.append(chr(sub))
+            c = (c + 1) & 0xff
+        outfile.write("".join(ctxt))
+    infile.close()
+    outfile.close()
+    end = time.time() - start
+    bps = fsize / end
+    sys.stdout.write("Completed in "+str(end)+" seconds\n")
+    sys.stdout.write(str(bps)+" bytes per second.\n")
+            
+            
+key = RedDye().kdf(key)
 
 if mode == "encrypt":
-    nonce = os.urandom(nonce_len)
-    c = reddye.crypt(data, key, nonce)
-    outfile.write(nonce+c)
+    encrypt(input_file, output_file, fsize, key)
 elif mode == "decrypt":
-    nonce = data[:nonce_len]
-    msg = data[nonce_len:]
-    plain_text = reddye.crypt(msg, key, nonce)
-    outfile.write(plain_text)
-outfile.close()
-
-end = time.time() - start
-bps = len(data) / end
-sys.stdout.write("Completed in "+str(end)+" seconds\n")
-sys.stdout.write(str(bps)+" bytes per second.\n")
+    decrypt(input_file, output_file, (fsize - nonce_len), key)
