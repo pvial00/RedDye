9e12d25e11d816bd9d86dd5850addb95fa252264
Karl Zander <pvial@Karls-MacBook-Pro.local>
2018-09-14 13:18:08 -0700

    RedCrypt with PBKDF2

--- c/redcrypt/make.sh
+++ c/redcrypt/make.sh
@@ -1,1 +1,1 @@
-gcc redcrypt.c -o reddye
+gcc redcrypt.c -lssl -lcrypto -I/usr/local/opt/openssl/include -L/usr/local/opt/openssl/lib -o reddye
--- c/redcrypt/redcrypt.c
+++ c/redcrypt/redcrypt.c
@@ -1,33 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "reddye_kdf_func.c"
+#include <openssl/evp.h>
 
 int keylen = 32;
-int k[32];
+//int k[] = { 83, 41, 201, 107, 175, 150, 205, 119, 188, 140, 154, 210, 72, 30, 194, 189, 130, 9, 134, 201, 11, 145, 29, 179, 192, 0, 125, 209, 184, 132, 207, 219 };
+int k[32] = {0};
 int j = 0;
 
 void keysetup(unsigned char *key, unsigned char *nonce) {
     int c;
     for (c=0; c < strlen(key); c++) {
-        k[c] = (k[c] + key[c]) % 0xff;
-        j = (j + k[c]) & 0xff; }
+        k[c] = (k[c] + key[c]) % 256;
+        j = (j + k[c]) % 256; }
     keylen = strlen(key);
     for (c = 0; c < 256; c++) {
-        k[c % keylen] = (k[c % keylen] + j) & 0xff;
-        j = (j + k[c % keylen]) & 0xff; }
+        k[c % keylen] = (k[c % keylen] + j) % 256;
+        j = (j + k[c % keylen]) % 256; }
     for (c = 0; c < strlen(nonce); c++) {
-        k[c] = (k[c] + nonce[c]) & 0xff;
-        j = (j + k[c]) & 0xff; }
+        k[c] = (k[c] + nonce[c]) % 256;
+        j = (j + k[c]) % 256; }
     for (c = 0; c < 256; c++) {
-        k[c % keylen] = (k[c % keylen] + j) & 0xff;
-        j = (j + k[c % keylen]) & 0xff; }
-}
-
-void expandpassword(unsigned char *password, unsigned char* key) {
-    for (int x = 0; x < strlen(password); x++) {
-        key[x] = 0;
-        key[x] = (key[x] + password[x]) % 256; }
+        k[c % keylen] = (k[c % keylen] + j) % 256;
+        j = (j + k[c % keylen]) % 256; }
 }
 
 void usage() {
@@ -73,17 +68,18 @@ int main(int argc, char *argv[]) {
         fread(&nonce, nonce_length, 1, randfile);
         fclose(randfile);
         fwrite(nonce, 1, nonce_length, outfile);
-        //expandpassword(password, key);
-        kdf(password);
+	unsigned char salt[] = "RedDyeCipher";
+	int iter = 10000;
+	PKCS5_PBKDF2_HMAC (password, sizeof(password) -1, salt, sizeof(salt)-1, iter, EVP_sha1(), keylen, key);
         keysetup(key, nonce);
         for (int d = 0; d < blocks; d++) {
             fread(block, buflen, 1, infile);
             bsize = sizeof(block);
             for (int b = 0; b < bsize; b++) {
-                k[c % keylen] = (k[c % keylen] + k[(c + 1) % keylen] + j) & 0xff;
-                j = (j + k[c % keylen] + c) & 0xff;
+                k[c % keylen] = (k[c % keylen] + k[(c + 1) % keylen] + j) % 256;
+                j = (j + k[c % keylen] + c) % 256;
                 block[b] = block[b] ^ k[c % keylen];
-                c = (c + 1) & 0xff;
+                c = (c + 1) % 256;
             }
             if (d == (blocks - 1) && extra != 0) {
                 bsize = extra;
@@ -98,17 +94,18 @@ int main(int argc, char *argv[]) {
             blocks += 1;
         }
         fread(nonce, 1, nonce_length, infile);
-        expandpassword(password, key);
-        kdf(password);
+	unsigned char salt[] = "RedDyeCipher";
+	int iter = 10000;
+	PKCS5_PBKDF2_HMAC (password, sizeof(password) -1, salt, sizeof(salt)-1, iter, EVP_sha1(), keylen, key);
         keysetup(key, nonce);
         for (int d = 0; d < blocks; d++) {
             fread(block, buflen, 1, infile);
             bsize = sizeof(block);
             for (int b = 0; b < bsize; b++) {
-                k[c % keylen] = (k[c % keylen] + k[(c + 1) % keylen] + j) & 0xff;
-                j = (j + k[c % keylen] + c) & 0xff;
+                k[c % keylen] = (k[c % keylen] + k[(c + 1) % keylen] + j) % 256;
+                j = (j + k[c % keylen] + c) % 256;
                 block[b] = block[b] ^ k[c % keylen];
-                c = (c + 1) & 0xff;
+                c = (c + 1) % 256;
             }
             if ((d == (blocks - 1)) && extra != 0) {
                 bsize = extra;
