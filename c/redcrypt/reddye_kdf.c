4a9ab259dcfee539c2368447e741e57010cc7732
Karl Zander <pvial@Karls-MacBook-Pro.local>
2018-09-13 18:49:29 -0700

    RedDye file encryption

--- c/redcrypt/reddye_kdf.c
+++ c/redcrypt/reddye_kdf.c
@@ -1,36 +1,36 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-    
-int kdf_keylen = 32;
-int kdf_k[] = { 83, 41, 201, 107, 175, 150, 205, 119, 188, 140, 154, 210, 72, 30, 194, 189, 130, 9, 134, 201, 11, 145, 29, 179, 192, 0, 125, 209, 184, 132, 207, 219 };
+
+int kkdf[32];
 int t = 0;
-int iterations = 10;
 
-void kdf_keysetup(unsigned char *key, unsigned char *nonce) {
+void keysetup(unsigned char *key, unsigned char *nonce) {
     int c;
-    for (c=0; c < sizeof(key); c++) {
-        kdf_k[c] = (kdf_k[c] + key[c]) % 256;
-        t = (t + kdf_k[c]) % 256; }
-    kdf_keylen = sizeof(key);
+    for (c=0; c < strlen(key); c++) {
+        kkdf[c] = (kkdf[c] + key[c]) & 0xff;
+        t = (t + kkdf[c]) & 0xff; }
+    keylen = strlen(kkdf);
     for (c = 0; c < 256; c++) {
-        kdf_k[c % kdf_keylen] = (kdf_k[c % kdf_keylen] + t) % 256;
-        t = (t + kdf_k[c % kdf_keylen]) % 256; }
+        kkdf[c % keylen] = (kkdf[c % keylen] + t) & 0xff;
+        t = (t + kkdf[c % keylen]) & 0xff; }
+    for (c = 0; c < strlen(nonce); c++) {
+        kkdf[c] = (kkdf[c] + nonce[c]) & 0xff;
+        t = (t + kkdf[c]) % 256; }
+    for (c = 0; c < 256; c++) {
+        kkdf[c % keylen] = (kkdf[c % keylen] + t) & 0xff;
+        t = (t + kkdf[c % keylen]) & 0xff; }
 }
 
-unsigned char * kdf_crypt(unsigned char *key, unsigned char *nonce, long flen) {
-   kdf_keysetup(key, nonce);
+
+unsigned char * crypt(unsigned char *data, unsigned char *key, unsigned char *nonce, long datalen) {
+   keysetup(key, nonce);
    int c = 0;
-   for (int x = 0; x < flen; x++) {
-       kdf_k[c % kdf_keylen] = (kdf_k[c % kdf_keylen] + kdf_k[(c + 1) % kdf_keylen] + t) % 256;
-       t = (t + kdf_k[c % kdf_keylen] + c) % 256;
-       key[x] = key[x] ^ kdf_k[c % kdf_keylen];
-       c = (c + 1) % 256; } return key; }
-
-unsigned char * kdf(unsigned char *key) {
-    unsigned char n[16] = "01234567890ABCDEF";
-    for (int x = 0; x < iterations; x++) {
-        kdf_crypt(key, n, kdf_keylen);
-    }
+   for (int x = 0; x < datalen; x++) {
+       kkdf[c % keylen] = (kkdf[c % keylen] + kkdf[(c + 1) % keylen] + t) & 0xff;
+       t = (t + kkdf[c % keylen] + c) & 0xff;
+       data[x] = data[x] ^ kkdf[c % keylen];
+       c = (c + 1) & 0xff;
+   } 
+   t = 0;
 }
-
