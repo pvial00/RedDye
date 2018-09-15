1b616caa9be26f9e56e48f2ea24e932dd8289d6c
Karl Zander <pvial@Karls-MacBook-Pro.local>
2018-09-15 12:05:41 -0700

    RedDye v1.0.1

--- c/redcrypt_pbkdf2/redcrypt.c
+++ c/redcrypt_pbkdf2/redcrypt.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "reddye.c"
+#include <openssl/evp.h>
 
 int keylen = 32;
 //int k[] = { 83, 41, 201, 107, 175, 150, 205, 119, 188, 140, 154, 210, 72, 30, 194, 189, 130, 9, 134, 201, 11, 145, 29, 179, 192, 0, 125, 209, 184, 132, 207, 219 };
@@ -11,18 +11,18 @@ int j = 0;
 void keysetup(unsigned char *key, unsigned char *nonce) {
     int c;
     for (c=0; c < strlen(key); c++) {
-        k[c] = (k[c] + key[c]) & 0xff;
-        j = (j + k[c]) & 0xff; }
+        k[c] = (k[c] + key[c]) % 256;
+        j = (j + k[c]) % 256; }
     keylen = strlen(key);
     for (c = 0; c < 256; c++) {
-        k[c % keylen] = (k[c % keylen] + j) & 0xff;
-        j = (j + k[c % keylen]) & 0xff; }
+        k[c % keylen] = (k[c % keylen] + j) % 256;
+        j = (j + k[c % keylen]) % 256; }
     for (c = 0; c < strlen(nonce); c++) {
-        k[c] = (k[c] + nonce[c]) & 0xff;
-        j = (j + k[c]) & 0xff; }
+        k[c] = (k[c] + nonce[c]) % 256;
+        j = (j + k[c]) % 256; }
     for (c = 0; c < 256; c++) {
-        k[c % keylen] = (k[c % keylen] + j) & 0xff;
-        j = (j + k[c % keylen]) & 0xff; }
+        k[c % keylen] = (k[c % keylen] + j) % 256;
+        j = (j + k[c % keylen]) % 256; }
 }
 
 void usage() {
@@ -42,8 +42,6 @@ int main(int argc, char *argv[]) {
     unsigned char *key[keylen];
     unsigned char *password;
     int nonce_length = 16;
-    int iterations = 10;
-    unsigned char *salt = "RedDyeCipher";
     unsigned char nonce[nonce_length];
     unsigned char block[buflen];
     if (argc != 5) {
@@ -51,6 +49,7 @@ int main(int argc, char *argv[]) {
     }
     mode = argv[1];
     in = argv[2];
+    out = argv[3];
     password = argv[4];
     infile = fopen(in, "rb");
     fseek(infile, 0, SEEK_END);
@@ -58,6 +57,7 @@ int main(int argc, char *argv[]) {
     fseek(infile, 0, SEEK_SET);
     outfile = fopen(out, "wb");
     int c = 0;
+    int i = 1;
     if (strcmp(mode, "encrypt") == 0) {
         long blocks = fsize / buflen;
         long extra = fsize % buflen;
@@ -68,16 +68,18 @@ int main(int argc, char *argv[]) {
         fread(&nonce, nonce_length, 1, randfile);
         fclose(randfile);
         fwrite(nonce, 1, nonce_length, outfile);
-	kdf(password, key, salt, iterations, keylen);
+	unsigned char salt[] = "RedDyeCipher";
+	int iter = 10000;
+	PKCS5_PBKDF2_HMAC (password, sizeof(password) -1, salt, sizeof(salt)-1, iter, EVP_sha1(), keylen, key);
         keysetup(key, nonce);
         for (int d = 0; d < blocks; d++) {
             fread(block, buflen, 1, infile);
             bsize = sizeof(block);
             for (int b = 0; b < bsize; b++) {
-                k[c % keylen] = (k[c % keylen] + k[(c + 1) % keylen] + j) & 0xff;
-                j = (j + k[c % keylen] + c) & 0xff;
+                k[c % keylen] = (k[c % keylen] + k[(c + 1) % keylen] + j) % 256;
+                j = (j + k[c % keylen] + c) % 256;
                 block[b] = block[b] ^ k[c % keylen];
-                c = (c + 1) & 0xff;
+                c = (c + 1) % 256;
             }
             if (d == (blocks - 1) && extra != 0) {
                 bsize = extra;
@@ -92,16 +94,18 @@ int main(int argc, char *argv[]) {
             blocks += 1;
         }
         fread(nonce, 1, nonce_length, infile);
-	kdf(password, key, salt, iterations, keylen);
+	unsigned char salt[] = "RedDyeCipher";
+	int iter = 10000;
+	PKCS5_PBKDF2_HMAC (password, sizeof(password) -1, salt, sizeof(salt)-1, iter, EVP_sha1(), keylen, key);
         keysetup(key, nonce);
         for (int d = 0; d < blocks; d++) {
             fread(block, buflen, 1, infile);
             bsize = sizeof(block);
             for (int b = 0; b < bsize; b++) {
-                k[c % keylen] = (k[c % keylen] + k[(c + 1) % keylen] + j) & 0xff;
-                j = (j + k[c % keylen] + c) & 0xff;
+                k[c % keylen] = (k[c % keylen] + k[(c + 1) % keylen] + j) % 256;
+                j = (j + k[c % keylen] + c) % 256;
                 block[b] = block[b] ^ k[c % keylen];
-                c = (c + 1) & 0xff;
+                c = (c + 1) % 256;
             }
             if ((d == (blocks - 1)) && extra != 0) {
                 bsize = extra;
