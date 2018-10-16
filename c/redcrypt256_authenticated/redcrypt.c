75072fa25130bc43b3824cdf991f611a4634369d
Karl Zander <pvial@Karls-MacBook-Pro.local>
2018-10-15 18:28:58 -0700

    RedDye Refit

--- c/redcrypt256_authenticated/redcrypt.c
+++ c/redcrypt256_authenticated/redcrypt.c
@@ -134,7 +134,7 @@ int main(int argc, char *argv[]) {
                 fread(block, buflen, 1, infile);
                 bsize = sizeof(block);
                 for (int b = 0; b < bsize; b++) {
-                    k[c] = (k[c] + k[(c + 1) % keylen] + j) & 0xff;
+                    k[c] = (k[c] + k[(c + 1) & 0xff] + j) & 0xff;
                     j = (j + k[c] + c) & 0xff;
 		    output = ((j + k[c]) & 0xff) ^ k[c];
                     block[b] = block[b] ^ output;
